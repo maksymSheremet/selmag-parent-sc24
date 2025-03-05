959cc70603c73dbcbf5917470cc7af9ee95bcceb
Mitos <MiToSSH@gmail.com>
2025-03-05 15:27:46 +0200

    SC24EP01

---
+++ manager-app/src/main/java/my/code/controller/ProductsController.java
@@ -0,0 +1,48 @@
+package my.code.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import my.code.controller.payload.NewProductPayload;
+import my.code.entity.Product;
+import my.code.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("catalogue/products")
+public class ProductsController {
+
+    private final ProductService productService;
+
+    @GetMapping("list")
+    public String getProductsList(Model model) {
+        model.addAttribute("products", this.productService.findAllProducts());
+        return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getNewProductPage() {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(@Valid NewProductPayload payload, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toArray()
+            );
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
+    }
+}
---
+++ manager-app/src/main/java/my/code/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package my.code.repository;
+
+import my.code.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
